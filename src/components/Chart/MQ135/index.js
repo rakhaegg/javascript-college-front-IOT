c08ad48dce926d8f172b77e45b29410661afd297
rakha <rakhae.g@gmail.com>
2022-06-07 09:44:05 +0700

    change

---
+++ src/components/Chart/MQ135/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+class DataMQ extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    componentDidMount(){
+
+    }
+    componentWillUnmount(){
+
+    }
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default DataMQ
